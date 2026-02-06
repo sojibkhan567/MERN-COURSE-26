dd7fc753010389c56be3455024f5190de1031999
Sajeeb Khan <sbhossain567@gmail.com>
2026-02-06 23:17:28 +0600

    add ejs template

--- first-app-26/index.js
+++ first-app-26/index.js
@@ -2,13 +2,18 @@ const express = require("express");
 const app = express();
 
 const port = 3000;
+// use json or urlencode data
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+//set view ejs
+app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
-    res.send("<h1>Hello world!</h1>");
+    res.render("welcome");
 });
 
-app.get("/search", (req, res) => {
-    res.send(req.query);
+app.post("/user", (req, res) => {
+    res.send(req.body);
 });
 
 app.listen(port, () =>  {
